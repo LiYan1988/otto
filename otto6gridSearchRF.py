74b4d9dfe2e8564566c80ff13bf7f3b602d0138d
Li Yan <li.yan.ly414@gmail.com>
2016-10-23 18:38:30 +0200

    Optimize hyperparameters for RF, min_samples_leaf not included, running...

--- otto6gridSearchRF.py
+++ otto6gridSearchRF.py
@@ -21,13 +21,13 @@ if __name__=='__main__':
 #    params['criterion'] = ['gini', 'entropy']
     params['max_depth'] = [6, 9, 12, 15, 18]
     params['min_samples_split'] = [2, 8, 16, 32]
-#    params['min_samples_leaf'] = [1, 5, 10]
+    params['min_samples_leaf'] = [1, 5, 10]
     params['max_features'] = [0.8, 0.9, 1]
     kf = cross_validation.StratifiedKFold(y_train, n_folds=n_cv, shuffle=True, 
         random_state=0)
     
-    rndcv = model_selection.RandomizedSearchCV(rf, params, n_iter=15,
-        scoring='neg_log_loss', cv=kf, verbose=1, random_state=0)
+    rndcv = model_selection.RandomizedSearchCV(rf, params, n_iter=5,
+        scoring='neg_log_loss', cv=kf, verbose=10, random_state=0)
     rndcv.fit(x_train_tfidf, y_train)
     search_results = pd.DataFrame(rndcv.cv_results_)
     search_results.to_csv('rf_randomSearchCV.csv')
