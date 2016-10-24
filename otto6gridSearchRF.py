6af2bb7a1a4cadaa37a3513f5d7e7a0a1d046a12
Li Yan <li.yan.ly414@gmail.com>
2016-10-24 13:46:58 +0200

    Optimize hyperparameters for RF, running...

--- otto6gridSearchRF.py
+++ otto6gridSearchRF.py
@@ -15,20 +15,21 @@ if __name__=='__main__':
         add_features(x_train, x_test)
         
     # max_depth=21, max_features=0.6, n_estimators=600
+    # max_depth=33, max_features=0.5, n_estimators=800: 0.50777
     n_cv = 3
     rf = ensemble.RandomForestClassifier(class_weight='balanced', n_jobs=7)
     params = {}
-    params['n_estimators'] = [550, 600, 750, 800]
+    params['n_estimators'] = [800, 850, 900, 950, 1000]
 #    params['criterion'] = ['gini', 'entropy']
-    params['max_depth'] = [21, 25, 27, 30, 33] # larger better
+    params['max_depth'] = [33, 36, 39, 42, 45] # larger better
 #    params['min_samples_split'] = [2, 8, 16, 32] # smaller better
 #    params['min_samples_leaf'] = [1, 5, 10] # smaller better
-    params['max_features'] = [0.5, 0.6, 0.7, 0.8] # smaller better
+    params['max_features'] = [0.3, 0.4, 0.5, 0.6] # smaller better
     kf = cross_validation.StratifiedKFold(y_train, n_folds=n_cv, shuffle=True, 
         random_state=0)
     
-    rndcv = model_selection.RandomizedSearchCV(rf, params, n_iter=45,
+    rndcv = model_selection.RandomizedSearchCV(rf, params, n_iter=30,
         scoring='neg_log_loss', cv=kf, verbose=10, random_state=0)
     rndcv.fit(x_train_tfidf, y_train)
     search_results = pd.DataFrame(rndcv.cv_results_)
-    search_results.to_csv('rf_randomSearchCV3.csv')
+    search_results.to_csv('rf_randomSearchCV4.csv')
