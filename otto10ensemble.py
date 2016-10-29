c0df7ca947551fb1cc7d1b02cf12b65a075d87ce
Li Yan <li.yan.ly414@gmail.com>
2016-10-29 21:28:55 +0200

    Layer 1 stacking, EXT, RF, LR, KNN on log(X+1)

--- otto10ensemble.py
+++ otto10ensemble.py
@@ -13,19 +13,19 @@ if __name__=='__main__':
         add_features(x_train, x_test)
         
     ext = ensemble.ExtraTreesClassifier(class_weight='balanced', n_jobs=8, 
-        n_estimators=700, max_depth=39, max_features=0.85, verbose=10)
+        n_estimators=500, max_depth=40, max_features=0.7, verbose=1)
     
     rf = ensemble.RandomForestClassifier(class_weight='balanced', n_jobs=8, 
-        n_estimators=950, max_depth=39, max_features=0.5, verbose=10)
+        n_estimators=300, max_depth=40, max_features=0.6, verbose=1)
     
     lr = linear_model.LogisticRegression(C=0.06, class_weight='balanced', 
         max_iter=1000, n_jobs=8, multi_class='multinomial', random_state=0, 
-        verbose=10, solver='sag')
+        verbose=0, solver='sag')
     
     knn = neighbors.KNeighborsClassifier()
     
-    estimators = [ext, rf, lr, knn]
+    estimators = [ext, rf, lr]
     megaclf = MegaClassifier(estimators, cv=5)
     megaclf.fit_predict_proba(x_train_log, y_train.values, x_test_log, 
         random_state=0)
-    megaclf.save_results('megaclf')
+    megaclf.save_results('megaclf1')
