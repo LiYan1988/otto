eb7f5a420faa25ac6729feb21de904f9ce3f9f04
Li Yan <li.yan.ly414@gmail.com>
2016-11-05 08:19:51 +0100

    CV repetitions are not very good, increase CV repetitions does not help

--- otto13bagging.py
+++ otto13bagging.py
@@ -5,7 +5,8 @@ Created on Sun Oct 30 12:20:38 2016
 @author: lyaa
 layer 1: [ext, rf, lr], layer 2: xgboost, cv=5, repetition=20, 
 LB private:0.42343
-
+layer 1: [ext, rf, lr, knns], layer 2: xgboost, cv=12, repetition=20, 
+LB private: 0.42011, increase cv doesn't improve results
 """
 
 from ottoStart import *
@@ -40,14 +41,14 @@ xgbclf = xgb.XGBClassifier(objective='multi:softprob', silent=False,
 estimators_layer1 = estimators
 estimators_layer2 = [xgbclf]
 megabag = MegaBagging(x_train_tfidf.toarray(), y_train.values, 
-    x_test_tfidf.toarray(), estimators_layer1, estimators_layer2, cv=5, 
+    x_test_tfidf.toarray(), estimators_layer1, estimators_layer2, cv=12, 
     repetitions_layer2=20)
 megabag.cv_repeat(0)
 
 if not os.path.exists('megabag1'):
     os.mkdir('megabag1')
     
-save_data(megabag.y_test_pred, 'megabag1/megabag1_final.pkl')
-save_data(megabag.train_new_x, 'megabag1/megabag1_train.pkl') 
-save_data(megabag.test_new_x, 'megabag1/megabag1_test.pkl')
+save_data(megabag.y_test_pred, 'megabag1/megabag3_final.pkl')
+save_data(megabag.train_new_x, 'megabag1/megabag3_train.pkl') 
+save_data(megabag.test_new_x, 'megabag1/megabag3_test.pkl')
 
